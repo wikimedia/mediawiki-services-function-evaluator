e2baa95f3d2f9dc1f6413e72abcf00a294340d06
dvrandecic <dvrandecic@wikimedia.org>
2020-10-09 10:42:43 -0700

    added some arguments for the command line

--- README.md
+++ README.md
@@ -1,5 +1,10 @@
 This is a simple evaluation engine for ZObjects.
 
+For parameters, run 
+```
+node src/function-evaluator.js --help
+```
+
 Run as:
 
 ```
@@ -27,7 +32,7 @@ node src/function-evaluator.js '{ "Z1K1": "Z7", "Z7K1": "Zxx", "ZxxK1": "A" }'
 - [x] do the tests look right?
 - [x] what should be the config for the linter?
 - [ ] fix representation and ZIDs of errors
-- [ ] read arguments
+- [x] read arguments
 - [ ] settings
 - [ ] hook it up to the wiki
 - [ ] make it runnable as a service
--- src/function-evaluator.js
+++ src/function-evaluator.js
@@ -2,24 +2,65 @@
 
 'use strict';
 
-console.log( 'zeval v0.0.1' );
-
 const canonicalize = require( './canonicalize.js' );
 const evaluate = require( './evaluate.js' );
 const normalize = require( './normalize.js' );
 const wellformed = require( './wellformed.js' );
 const parse = require( './parse.js' );
 
-console.log(
-	JSON.stringify(
-		canonicalize(
-			evaluate(
-				normalize(
-					wellformed(
-						parse( process.argv[ 2 ] )
-					)
-				)
-			)
-		), null, 2
-	)
-);
+// process arguments
+let input;
+let ok = false;
+let normal = false;
+let do_eval = true;
+if ( process.argv.length < 3 ) {
+	console.log( 'function-evaluator v0.0.1' );
+	console.log( 'See --help for help' );
+}
+for ( let i = 2; i < process.argv.length; i++ ) {
+	const arg = process.argv[ i ];
+
+	if ( arg === '--help' ) {
+		console.log( 'Needs a ZObject as an argument, e.g.' );
+		console.log( 'function-evaluator.js \'{ "Z1K1": "Z6", "Z6K1": "text" }\'' );
+		console.log( 'Available parameters:' );
+		console.log( '  --help    for showing this help' );
+		console.log( '  --normal  for normalizing the result (otherwise canonical)' );
+		console.log( '  --noeval  for not evaluating the parse (default is to evaluate)' );
+		continue;
+	}
+
+	if ( arg === '--normal' ) {
+		normal = true;
+		continue;
+	}
+
+	if ( arg === '--noeval' ) {
+		do_eval = false;
+		continue;
+	}
+
+	if ( input === undefined ) {
+		input = arg;
+		ok = true;
+		continue;
+	}
+
+	console.log( 'Unknown parameter: ' + arg );
+	input = '';
+	ok = false;
+}
+
+if ( ok ) {
+	let result = normalize( wellformed( parse( input ) ) );
+
+	if ( do_eval ) {
+		result = canonicalize( evaluate( result ) );
+	}
+
+	if ( normal ) {
+		result = normalize( result );
+	}
+
+	console.log( JSON.stringify( result, null, 2 ) );
+}
