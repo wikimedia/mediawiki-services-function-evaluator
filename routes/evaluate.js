2c4cbbed3eb29d3dd52577c24d1d46044235ce03
apine <cmassaro@wikimedia.org>
2022-12-19 15:25:13 -0500

    Add 15-second timeout to the evaluator.
    Bug: T323046
    Change-Id: I8f190202a31310d470a9a30e6f9955d92c2b2fcb

--- routes/evaluate.js
+++ routes/evaluate.js
@@ -2,6 +2,7 @@
 const sUtil = require( '../lib/util' );
 const { maybeRunZ7 } = require( '../src/maybeRunZ7.js' );
 const { getWrappedZObjectFromVersionedBinary, getZObjectFromBinary } = require( '../executors/javascript/function-schemata/javascript/src/serialize.js' );
+const { makeMappedResultEnvelope } = require( '../executors/javascript/function-schemata/javascript/src/utils.js' );
 
 /**
  * The main router object
@@ -12,25 +13,56 @@ const router = sUtil.router();
  * The main application object reported when this module is require()d
  */
 let app;
+let childProcess = null;
+
+async function propagateResult( res, result ) {
+	// Kill the executor child process if it has survived.
+	if ( childProcess !== null ) {
+		try {
+			process.kill( childProcess.pid );
+			childProcess = null;
+		} catch ( error ) { }
+	}
+	res.json( result );
+}
 
 async function runFunctionCall( ZObject, res, websocket = null ) {
 	const resultTuple = await maybeRunZ7( ZObject, websocket );
 
-	// Kill the executor child process if it has survived.
-	const childProcess = resultTuple.process;
-	if ( childProcess !== null ) {
-		try {
-			process.kill( childProcess.pid );
-		} catch ( error ) { }
-	}
+	childProcess = resultTuple.process;
 
 	// Return the resulting Z22 to the caller.
-	res.json( resultTuple.Z22 );
+	propagateResult( res, resultTuple.Z22 );
 }
 
 router.post( '/', async ( req, res ) => {
+
 	const wss = app.wss;
 	let ZObject;
+
+	setTimeout(
+		function () {
+			propagateResult(
+				res,
+				makeMappedResultEnvelope(
+					null,
+					{
+						Z1K1: {
+							Z1K1: 'Z9',
+							Z9K1: 'Z5'
+						},
+						// TODO (T292804): Figure out what error this should actually be.
+						Z5K1: {
+							Z1K1: 'Z6',
+							Z6K1: 'Function call timed out'
+						}
+					}
+				)
+			);
+		},
+		// TODO (T325590): Parameterize this.
+		15000
+	);
 
 	try {
 		// Case 1: The ZObject was serialized with version 0.0.1 of the
